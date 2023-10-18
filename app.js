965a57d630f39fc996915661652190097d198b38
FedericaSidoti <federicasidoti21@gmail.com>
2023-10-18 16:36:36 +0200

    ADD- stamp in page

--- app.js
+++ app.js
@@ -3,40 +3,56 @@ const team = [
     {
         nome: 'Wayne Barnett',
         ruolo: 'Founder & CEO', 
-        foto: 'wayne-barnett-founder-ceo.jpg'
+        foto: './img/wayne-barnett-founder-ceo.jpg'
     },
     {
         nome: 'Angela Caroll',
         ruolo: 'Chief Editor', 
-        foto: 'angela-caroll-chief-editor.jpg'
+        foto: './img/angela-caroll-chief-editor.jpg'
     },
     {
         nome: 'Walter Gordon',
         ruolo: 'Office Manager', 
-        foto: 'walter-gordon-office-manager.jpg'
+        foto: './img/walter-gordon-office-manager.jpg'
     },
     {
         nome: 'Angela Lopez',
         ruolo: 'Social Media Manager', 
-        foto: 'angela-lopez-social-media-manager.jpg'
+        foto: './img/angela-lopez-social-media-manager.jpg'
     },
     {
         nome: 'Scott Estrada',
         ruolo: 'Developer', 
-        foto: 'scott-estrada-developer.jpg'
+        foto: './img/scott-estrada-developer.jpg'
     },
     {
         nome: 'Barbara Ramos',
         ruolo: 'Graphic Designer', 
-        foto: 'barbara-ramos-graphic-designer.jpg'
+        foto: './img/barbara-ramos-graphic-designer.jpg'
     }
 ]
 
 //console.log(team)
-//Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+//Stampare per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+const cardDomElement = document.querySelector('.card');
+
 for(let i = 0; i < team.length; i++) {
     const currentMember = team[i] 
     console.log(currentMember.nome)
     console.log(currentMember.ruolo)
     console.log(currentMember.foto)
+
+    const currentIMG = currentMember.foto;
+    const htmlString = `
+    <div class="img">
+        <img src="${currentIMG}">
+    </div>
+    <div class="text">${currentMember.nome}</div>
+    <div class="text">${currentMember.ruolo}</div>
+    `
+
+    cardDomElement.innerHTML += htmlString
 }
+
+// - PER OGNI immagine, creare un div che la contenga e inserirlo nell'html
+
